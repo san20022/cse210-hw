2295609b7401d027b1483a6a220bfd5e9c79e01a
san20022 <san20022@byui.edu>
2025-10-20 19:16:48 -0600

    Program 3 Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        while (guess != magicNumber)
+        {
+            Console.Write("what is your guess? ");
+            string answer = Console.ReadLine();
+            guess = int.Parse(answer);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
