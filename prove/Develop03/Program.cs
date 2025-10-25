92f00ed5191d643088d6fb35e7e060b8871a2977
san20022 <san20022@byui.edu>
2025-10-25 14:49:34 -0600

    Build code

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Proverbs", 3, 5, 6)
+        string tex = "Trust in the lord with all your heart and lean not on your own understanding. " +
+                     "In all your ways acknowledge him, and he shall direct your paths.";
+        Scripture scripture = new Scripture(reference, tex);
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine(""
+        }
     }
 }
