dcb447bf06d8c5cc27c898a5900c6a7a520f55e9
san20022 <san20022@byui.edu>
2025-11-09 18:10:56 -0700

    update

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace MindfulnessProgram
+{
+    class BreathingActivity : Activity
+    {
+        public BreathingActivity()
+            : base("Breathing activity",
+                   "This Activity will help you relax by guiding you to breathe slowly and deeply.")
+        {
+        }
+
+        public override void Run()
+        {
+            DispplayStartingMessage();
+            DateTime end = DateTime.Now.AddSeconds(_duration);
+            bool breathIn = true;
+
+            while (DateTime.Now < end)
+            {
+                Console.WriteLine();
+                Console.Write(breathIn ? "Breathe in... " : " Breath out...");
+                ShowCountDown(breathIn ? 4 : 6):
+                breatheIn = !breatheIn;
+            }
+
+            DisplayEndingMessage();
+        }           
+    }
+}
