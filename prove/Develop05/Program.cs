13e6a68af41d6dc6dcfff1a5f97e58dbf9dc3db0
san20022 <san20022@byui.edu>
2025-11-23 15:21:01 -0700

    updates

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -58,5 +58,75 @@ class Program
                     break;    
             }
         }
+
+        Console.WriteLine("Thanks for using Eternal Quest. Goodbye!");
+    }
+
+    static void CreateGoalUI(GoalManager gm)
+    {
+        GoalManager.PrintGoalTypes();
+        Console.Write("Choose goal type 1-3): ");
+        string t = Console.ReadLine()?.Trime();
+        switch (t)
+        {
+            case "1":
+                Console.Write("Name: ");
+                var nameS = Console.ReadLine();
+                Console.Write("Description: ");
+                var descS = Console.ReadLine();
+                Console.Write("Points (integer): ");
+                int ptsS = ReadIntFromConsole(100);
+                gm.AddGoal(new SimpleGoal(nameS, descS, ptsS));
+                Console.Writeline("Simple goals created.");
+                break;
+            case "2":
+                Console.Write("Name:");
+                var nameE = Console.ReadLine);
+                Console.Write("Description: ");
+                var descE = Console.ReadLine();
+                Console.Write("Points per event (intteger): ");
+                int ptsE = ReadIntFromConsole(10);
+                gm.AddGoal(new EternalGoal(nameE, descE, ptsE));
+                Console.WriteLine("Eternal goal created.");
+                break;
+            case "3":
+                Console.Write("Name: ");
+                var nameC = Console.ReadLine();
+                Console.Write("Description");
+                var descC = Console.ReadLine();
+                Console.Write("Points per event (integer): ");
+                int ptsC = ReadIntFromConsole(10);
+                Console.WriteLine("Target count (how many times to comlete): ");
+                int target = ReadIntFromConsole(5);
+                Console.Write("Bonus at completion (integer): ");
+                int bonus = ReadIntFromConsole(50);
+                gm.AddGoal(new CheckListGoal(nameC, descC, ptsC, target, bonus));
+                Console.WriteLine("Checklist goal created.");
+                break;
+            default:
+                Console.Writeline("Invalid type.");
+                break;
+        }
+    }
+
+    static void RecordEventUI(GoalManager gm)
+    {
+        gm.ShowGoals();
+        Console.Write("Enter the goal number you completed (0 to cancel); ");
+        int sel = ReadIntFromConsole(0);
+        if (sel == 0) return;
+        gm.RecordEvent(sel);
+    }
+    static int ReadIntFromConsole(int defaultVal)
+    {
+        string s = Console.ReadLine();
+        if (int.TryParsel(s, out int val)) return val;
+        return defaultVal;
+    }
+
+    static void AddDemoGoals(GoalManager gm)
+    {
+        gm.AddGoal(new SimpleGoal("Run a marathon", "Finish a full marathon", 1000));
+        
     }
 }
