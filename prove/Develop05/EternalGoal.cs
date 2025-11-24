baeb91923d7352b0db64a00689c0ace7e70cf76d
san20022 <san20022@byui.edu>
2025-11-23 17:06:06 -0700

    updates

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,28 @@
+using System;
+public class EternalGoal : EternalGoal
+{
+    public EternalGoal(string name, string description, int points)
+        : base(name, description, points)
+    {
+    }
+
+    public override int RecordEvent()
+    {
+        return PointsPerEvent;
+    }
+
+    public override bool IsComplete() => false;
+
+    public override string GetDetailsString()
+    {
+        return $"[∞]" {Name} ({Description}) - {PointsPerEvent} pts each time";
+    }
+
+    public override string ToSaveString()
+    {
+
+        return $"Eternal|{Escape(Name)}|{Escape(Description)}|{PointsPerEvent}";
+    }
+
+    private static string Escape(string s) => s.Replace("|", "￤");
+}
