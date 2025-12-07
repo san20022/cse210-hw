ce3c8e0182bb4e130630337f53a0987df2904c8f
san20022 <san20022@byui.edu>
2025-12-07 15:20:28 -0700

    updates

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -26,8 +26,32 @@ class Program
         video1._comments.Add(comment1_3);
         Comment comment1_4 = new Comment("commentator1_4", "Meh, there are better videos.")
         video1._comments.Add(comment1_4);
-        
+        Comment comment2_1 = new Comment("commentator2_1", "This is not so great.");
+        video2._comments.Add(comment2_1);
+        Comment comment2_2 = new Comment("commentator2_2", "So disappointing!");
+        video2._comments.Add(comment2_2);
+        Comment comment2_3 = new Comment("commentator2_3", "Loved it!");
+        video2._comments.Add(comment2_3);
+        Comment comment3_1 = new Comment("commentator3_1", "WOW!!!!");
+        video3._comments.Add(comment3_1);
+        Comment comment3_2 = new Comment("commentator3_2", "I heart this!");
+        video3._comments.Add(comment3_2);
+        Comment comment3_3 = new Comment("commentator3_3", "CCCRRRAAAZZZYYY!!!");
+        video3._comments.Add(comment3_3);
+        Comment comment4_1 = new Comment("commentator3_1", "Wow, that wasted 40 seconds of my life.");
+        video4._comments.Add(comment4_1);
+        Comment comment4_2 = new Comment("commentator3_2", "I heart this!");
+        video4._comments.Add(comment4_2);
+        Comment comment4_3 = new Comment("commentator3_3", "CCCRRRAAAZZZYYY!!!");
+        video4._comments.Add(comment4_3);
+        Comment comment4_4 = new Comment("commentator3_4", "Who would have thought?!?!?!?");
+        video4._comments.Add(comment4_4);
 
-        
-    }
+        foreach (Video video in videoCollection)
+        {
+            Console.Write($"{video.GetVideoName()}, by {video.GetAuthor()}, is {video.GetLength()} seconds long, and has {video.NumberOfComments()} comments.\n");
+            video.DisplayListOfComments();
+            Console.Write("\n")
+        }
+    }   
 }
