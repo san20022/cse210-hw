f8c90e3f22cd5198120811df78199dcaf4fd6481
san20022 <san20022@byui.edu>
2025-12-06 19:39:25 -0700

    Update

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,21 @@
 using System;
-
+using System.Text.RegularExpressions;
 class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
         Console.WriteLine("Hello Foundation1 World!");
+
+        List<Video> videoCollection = new List<Video>();
+
+        Video video1 = new Video("title1", "author1", 10);
+        videoCollection.Add(video1);
+        Video video2 = new Video("title2", "author2", 20);
+        videoCollection.Add(video2);
+        Video video3 = new Video("title3", "author3", 30);
+        videoCollection.Add
+
+        
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,12 @@
 using System;
-
+using System.Reflection;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation2 World!");
+        Console.Clear();
+        Console.CursorVisible = false;
+        Product product1 = new Product("productA", "A-123ABC", 10)
     }
 }
