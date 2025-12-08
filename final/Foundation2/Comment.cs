55cbf24eec5f9a3f1f5bc9eb7099cdc13f2b84de
san20022 <san20022@byui.edu>
2025-12-07 20:12:53 -0700

    update

---
+++ final/Foundation2/Comment.cs
@@ -0,0 +1,15 @@
+using System;
+class Comment
+{
+    private string _name = "";
+    private string _comment = "";
+    public Comment(string name, string comment)
+    {
+        _name = name
+        _comment = comment;
+    }
+    public void Display()
+    {
+        Console.WriteLine($"{_name} - {_comment}");
+    }
+}
